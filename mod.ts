de78acb7a28c4e30049912dea3b617aeb89d085f
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2021-09-01 23:22:29 -0300

    Troca content type para text/html

--- mod.ts
+++ mod.ts
@@ -6,7 +6,7 @@ addEventListener('fetch', event => {
   event.respondWith(
     new Response(`Seu número da sorte de hoje é: ${generate()}`, {
       status: 200,
-      headers: { 'content-type': 'text/plain' }
+      headers: { 'content-type': 'text/html' }
     })
   )
 })
